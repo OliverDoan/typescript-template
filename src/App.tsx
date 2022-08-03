fb9452522775063907821bf409b8b4fbff15f36d
OliverDoan <doanvietthuan1005@gmail.com>
2022-08-03 23:04:49 +0700

    init project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { About } from '@components/About'
+
+import '@styles/app.scss'
+function App() {
+  return (
+    <>
+      <h1>Hello typescipt</h1>
+      <About />
+    </>
+  )
+}
+
+export default App
